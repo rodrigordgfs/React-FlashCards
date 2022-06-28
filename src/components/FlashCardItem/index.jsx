898d27e1ce2977d6a5e0c53ead2a959b9bbb1d9f
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2022-06-28 18:21:39 -0300

    Adicionado Tooltip nos icones do FlashCardItem

--- src/components/FlashCardItem/index.jsx
+++ src/components/FlashCardItem/index.jsx
@@ -2,6 +2,7 @@ import {
   AiOutlineEdit as EditIcon,
   AiOutlineDelete as DeleteIcon,
 } from "react-icons/ai";
+import ReactTooltip from 'react-tooltip';
 
 export default function FlashCardItem({
   flashcard = {},
@@ -36,12 +37,16 @@ export default function FlashCardItem({
           onClick={handleEditFlashcard}
           className="cursor-pointer"
           size={24}
+          data-tip="Editar FlashCards"
         />
         <DeleteIcon
           onClick={handleDeleteFlashcard}
           className="cursor-pointer"
           size={24}
+          data-tip="Deletar FlashCards"
         />
+        <ReactTooltip />
+
       </div>
     </div>
   );
